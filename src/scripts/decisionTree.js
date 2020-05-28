6c4283ee450737b805bf65430f42883aee108cbf
leandrocodes <leandrovianacodes@gmail.com>
2020-05-28 07:34:13 -0400

    faltando startar

--- src/scripts/decisionTree.js
+++ src/scripts/decisionTree.js
@@ -1,9 +1,14 @@
 const DecisionTree = () => {
-	return {
-		create: movie => ({
-			movie,
-			questions: {},
-			questionsArray: []
-		})
-	}
+    return {
+        create: movie => ({
+            movie,
+            questions: {},
+            questionsArray: [],
+
+            addQuestion(name, condition, callback1, callback2, callback3) {
+                this.questions[name] = { condition, callback1, callback2, callback3 }
+                this.questionsArray.push({ condition, callback1, callback2, callback3 })
+            }
+        })
+    }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,25 @@
 const decisionTree = DecisionTree()
 
+let success
+
 const id3 = decisionTree.create({
-	origin: 'br',
-	bigStar: false,
-	genre: 'comedy'
+    origin: 'br',
+    bigStar: false,
+    genre: 'comedy'
 })
 
-console.log(id3)
+id3.addQuestion(
+    'origin',
+    id3 => id3.origin == 'br',
+    () => {
+        success = false
+    },
+    () => {
+        success = false
+    },
+    () => {
+        success = false
+    }
+)
+
+console.log(id3, success)
